049a4686cd0a281fc0a9f955291eb2455eb784ef
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-09-09 00:27:33 -0500

    ejemplo de formatear una hora con DateTimeFormatter

---
+++ src/main/java/com/jhanatan/datetime/ejemplos/EjemploFormatearHora.java
@@ -0,0 +1,47 @@
+package com.jhanatan.datetime.ejemplos;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
+public class EjemploFormatearHora {
+
+    public static void main(String[] args) {
+        System.out.println("EJEMPLO LOCAL TIME");
+
+        //cremos la hora 
+        LocalTime aAhora = LocalTime.now();
+
+        System.out.println(aAhora);
+        System.out.println("Hora: " + aAhora.getHour());
+        System.out.println("Minutos: " + aAhora.getMinute());
+        System.out.println("Seg: " + aAhora.getSecond());
+
+        System.out.println("");
+
+        //otra forma de crear una hora con segundos
+        LocalTime onceDiecisiete = LocalTime.of(11, 17, 50);
+        System.out.println("Hora: " + onceDiecisiete);
+
+        //establecemos la hora
+        onceDiecisiete = LocalTime.parse("11:57");
+        System.out.println(onceDiecisiete);
+
+        //sumando a la hora
+        LocalTime seisDoce = LocalTime.of(6, 30).plus(1, ChronoUnit.HOURS);
+        System.out.println("Nueva Hora: " + seisDoce);
+
+        //para ver si una hora es anteior a la otra con segundos
+        boolean esAnterior = LocalTime.of(6, 12, 50).isBefore(LocalTime.parse("07:30"));
+        System.out.println("Es anterior: " + esAnterior);
+
+        DateTimeFormatter df = DateTimeFormatter.ofPattern("hh:mm:ss a");
+        String hora = df.format(onceDiecisiete);
+        String hora2 = onceDiecisiete.format(df);
+
+        System.out.println("Hora formateada: " + hora2);
+        System.out.println("Hora formateada: " + hora);
+
+    }
+
+}
