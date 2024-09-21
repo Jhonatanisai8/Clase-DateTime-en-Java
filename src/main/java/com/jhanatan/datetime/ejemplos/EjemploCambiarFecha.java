fcf05f55ff8309ff199d1ee86ed103813557681c
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-09-21 10:52:39 -0500

    ejemplo cambiarmos horas

--- src/main/java/com/jhanatan/datetime/ejemplos/EjemploCambiarFecha.java
+++ src/main/java/com/jhanatan/datetime/ejemplos/EjemploCambiarFecha.java
@@ -1,13 +1,15 @@
 package com.jhanatan.datetime.ejemplos;
 
+import java.time.Duration;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.Period;
 
 public class EjemploCambiarFecha {
 
     public static void main(String[] args) {
         System.out.println("EJEMPLO PERIDO");
-        ejempl01();
+        ejemplo02();
     }
 
     public static void ejempl01() {
@@ -26,5 +28,25 @@ public class EjemploCambiarFecha {
 
         System.out.printf("Periodo entre %s y %s hay %d años, %d meses y %d dias",
                 fecha01, fecha03, period01.getYears(), period01.getMonths(), period01.getDays());
+    }
+
+    public static void ejemplo02() {
+        //creamos una fecha 
+        LocalDateTime fecha01 = LocalDateTime.now();
+        //le establecemos una hora
+        fecha01 = fecha01.withHour(12);
+        //a la segunda fecha le agregamos tiempo
+        LocalDateTime fecha02 = LocalDateTime.now().plusHours(3).plusMinutes(45).plusDays(4);
+
+        //calculamos el tiempo transcurrido 
+        Duration lapsus = Duration.between(fecha01, fecha02);
+        System.out.println("Lapsus entre fecha 01 y 02: " + lapsus);
+
+        //extraemos los segundos,minutos, dias ect en el determinado tiempo 
+        System.out.println("Minutos: " + lapsus.toMinutes());
+        System.out.println("Segundos : " + lapsus.getSeconds());
+        System.out.println("Dias: " + lapsus.toDays());
+        System.out.println("Horas: " + lapsus.toHours());
+        System.out.println("Horas despues de 5: " + lapsus.plusHours(5));
     }
 }
